a01574e2965e797784d44480585971cf7534718d
pppurple <shuggie.otis0@gmail.com>
2016-08-05 01:23:40 +0900

    add ProjectCoinExample

---
+++ javase7/src/main/java/javase7/ProjectCoinExample.java
@@ -0,0 +1,7 @@
+package javase7;
+
+/**
+ * Created by pppurple on 2016/07/27.
+ */
+public class ProjectCoinExample {
+}
---
+++ javase7/src/test/java/javase7/ProjectCoinExampleTest.java
@@ -0,0 +1,96 @@
+package javase7;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.*;
+
+/**
+ * Created by pppurple on 2016/07/27.
+ */
+public class ProjectCoinExampleTest {
+    @Test
+    public void swithで文字列が使用できる() throws Exception {
+        String str = "bar";
+
+        switch (str) {
+            case "foo":
+                str += "1";
+                break;
+            case "bar":
+                str += "2";
+                break;
+            case "foobar":
+                str += "3";
+                break;
+            default:
+                str += "4";
+        }
+        assertThat(str, is("bar2"));
+    }
+
+    @Test
+    public void 二進数リテラルが使用できる() throws Exception {
+        int a = 0b0100;
+
+        assertThat(a, is(4));
+    }
+
+    @Test
+    public void 数値リテラルで区切り文字が使用できる() throws Exception {
+        int a = 1_000_000;
+        int b = 0b0001_1000;
+
+        assertThat(a, is(1000000));
+        assertThat(b, is(24));
+    }
+
+    @Test
+    public void 例外のマルチキャッチが可能() throws Exception {
+        String message = "";
+        try {
+            Class clazz = Class.forName("Sample");
+        } catch (ClassNotFoundException | ClassCastException e) {
+            message = e.getMessage();
+        }
+
+        assertThat(message, is("Sample"));
+    }
+
+    @Test
+    public void ダイヤモンド演算子が使用できる() throws Exception {
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+
+        assertThat(list, hasItems("aaa", "bbb", "ccc"));
+    }
+
+    @Test
+    public void リソースが自動クローズされる() throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/java/javase7/samplefile.txt"))) {
+            for(;;) {
+                String line = reader.readLine();
+                if (line != null) {
+                    sb.append(line);
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertThat(sb.toString(), is("abcdefg"));
+    }
+}
