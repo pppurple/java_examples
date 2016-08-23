764328f93ef4caaae264c5a51b5b276ca694fcae
pppurple <shuggie.otis0@gmail.com>
2016-08-24 00:11:50 +0900

    update MethodReferenceExampleTest

--- javase8/src/test/java/javase8/MethodReferenceExampleTest.java
+++ javase8/src/test/java/javase8/MethodReferenceExampleTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
+import java.util.Random;
+import java.util.function.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -17,7 +18,7 @@ import static org.junit.Assert.assertThat;
  */
 @RunWith(Enclosed.class)
 public class MethodReferenceExampleTest {
-    public static class MethodReference {
+    public static class ClassMethodReference {
         @Test
         public void メソッド参照() throws Exception {
             List<String> list = new ArrayList<>();
@@ -37,10 +38,10 @@ public class MethodReferenceExampleTest {
         @Test
         public void クラスメソッド参照() throws Exception {
             // ラムダ式
-            Consumer<String> consumer = str -> MethodReference.printWithBrace(str);
+            Consumer<String> consumer = str -> ClassMethodReference.printWithBrace(str);
             consumer.accept("abc");
             // メソッド参照
-            Consumer<String> consumer2 = MethodReference::printWithBrace;
+            Consumer<String> consumer2 = ClassMethodReference::printWithBrace;
             consumer2.accept("abc");
         }
 
@@ -50,10 +51,107 @@ public class MethodReferenceExampleTest {
 
         @Test
         public void 複数引数のクラスメソッド参照() throws Exception {
-            BiConsumer<String, String> bi = (pre, suf) -> MethodReference.printWithBi(pre, suf);
+            // ラムダ式
+            BiConsumer<String, String> bi = (pre, suf) -> ClassMethodReference.printWithBi(pre, suf);
             bi.accept("aaa", "bbb");
+            // メソッド参照
+            BiConsumer<String, String> bi2 = ClassMethodReference::printWithBi;
+            bi2.accept("aaa", "bbb");
+        }
+    }
 
-            BiConsumer<String, String> bi2 =
+    public static class InstanceMethodReference {
+        @Test
+        public void インスタンスメソッド参照() throws Exception {
+            // ラムダ式
+            List<String> list = new ArrayList<>();
+            Consumer<String> consumer = str -> list.add(str);
+            consumer.accept("abc");
+            // インスタンスメソッド参照
+            Consumer<String> consumer2 = list::add;
+            consumer2.accept("def");
+
+            String actual = list.get(0);
+            String expected = "abc";
+            assertThat(actual, is(expected));
+
+            String actual2 = list.get(1);
+            String expected2 = "def";
+            assertThat(actual2, is(expected2));
+        }
+
+        @Test
+        public void クラス名を指定したインスタンスメソッド参照() throws Exception {
+            // ラムダ式
+            Function<String, String> func = str -> str.toUpperCase();
+            String UpperStr = func.apply("abc");
+            // インスタンスメソッド参照
+            Function<String, String> func2 = String::toUpperCase;
+            String UpperStr2 = func2.apply("abc");
+
+            String expected = "ABC";
+            assertThat(UpperStr, is(expected));
+            assertThat(UpperStr2, is(expected));
+        }
+    }
+
+    public static class ConstructorReference {
+        @Test
+        public void コンストラクタ参照() throws Exception {
+            // ラムダ式
+            Supplier<Random> supplier = () -> new Random();
+            Random rand = supplier.get();
+            // コンストラクタ参照
+            Supplier<Random> supplier2 = Random::new;
+            Random rand2 = supplier2.get();
+
+            rand.setSeed(10L);
+            rand2.setSeed(10L);
+            int actual = rand.nextInt();
+            int actual2 = rand2.nextInt();
+            int expected = -1157793070;
+            assertThat(actual, is(expected));
+            assertThat(actual2, is(expected));
+        }
+
+        @Test
+        public void ジェネリクスを使用したコンストラクタ参照() {
+            // ラムダ式
+            Supplier<List<String>> supplier = () -> new ArrayList<>();
+            List<String> list = supplier.get();
+            // コンストラクタ参照
+            Supplier<List<String>> supplier2 = ArrayList<String>::new;
+            List<String> list2 = supplier2.get();
+        }
+
+        @Test
+        public void 引数のあるコンストラクタ参照() {
+            // ラムダ式
+            Function<Long, Random> func = (seed) -> new Random(seed);
+            Random rand = func.apply(10L);
+            // コンストラクタ参照
+            Function<Long, Random> func2 = Random::new;
+            Random rand2 = func2.apply(10L);
+
+            int actual = rand.nextInt();
+            int actual2 = rand2.nextInt();
+            int expected = -1157793070;
+            assertThat(actual, is(expected));
+            assertThat(actual2, is(expected));
+        }
+
+        @Test
+        public void 配列生成() {
+            // ラムダ式
+            IntFunction<String[]> func = (size) -> new String[size];
+            String[] arr = func.apply(5);
+            // コンストラクタ参照
+            IntFunction<String[]> func2 = String[]::new;
+            String[] arr2 = func2.apply(5);
+
+            int length = 5;
+            assertThat(arr.length, is(length));
+            assertThat(arr2.length, is(length));
         }
     }
 }
