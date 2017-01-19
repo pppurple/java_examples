1cfe8d25aed4b94c878e6974ee795b4ccfb7a322
purple <shuggie.otis0@gmail.com>
2017-01-20 01:16:20 +0900

    [WIP] add ftpserver

---
+++ ftpserver/src/main/java/ftpserver/FtpServerThread.java
@@ -0,0 +1,113 @@
+package ftpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class FtpServerThread implements Runnable {
+    private static String separator; // file separator
+    private static String dir;
+    private Socket client;
+
+    static {
+        separator = System.getProperty("file.separator");
+        dir = System.getProperty("user.dir");
+    }
+
+    FtpServerThread(Socket client) {
+        this.client = client;
+    }
+
+    public void run() {
+        try {
+            int ip = -1;
+
+            InetAddress clientInet = client.getInetAddress();
+            String host = clientInet.toString();
+            int idx = host.indexOf("/");
+            host = host.substring(idx + 1);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
+            out.println("220 Welcome to simple server.\r");
+
+            boolean done = false;
+            while(!done){
+                String line = in.readLine();
+                System.out.println("[host: " + host + "]" + line) ;
+
+                if(line == null){
+                    done = true;
+                    break;
+                }
+                if(line.startsWith("USER")){
+                    out.println("331 Password required for username.\r");
+                }
+                if(line.startsWith("PASS")){
+                    out.println("230 Login successful.\r");
+                }
+                if(line.startsWith("SYST")){
+                    out.println("215 UNIX Type: L8.\r");
+                }
+                if(line.startsWith("TYPE")){
+                    out.println("200 Switching to $line mode.\r");
+                }
+
+                if(line.startsWith("PORT")){
+                    out.println("200 PORT command successful.\r");
+                    String a1 = "";
+                    String a2 = "";
+                    int lng = line.length() - 1;
+                    int lng2 = line.lastIndexOf(",");
+                    int lng1 = line.lastIndexOf(",", lng2 - 1);
+                    for (int i = lng1 + 1; i < lng2; i++) {
+                        a1 = a1 + line.charAt(i);
+                    }
+                    for (int i = lng2 + 1; i <= lng; i++) {
+                        a2 = a2 + line.charAt(i);
+                    }
+                    int ip1 = Integer.parseInt(a1);
+                    int ip2 = Integer.parseInt(a2);
+                    ip = ip1 * 16 * 16 + ip2;
+                }
+                if (line.startsWith("STOR")) {
+                    out.println("150 Binary data connection");
+                    String file = line.substring(4).trim();
+                    System.out.println("file:" + file);
+                    System.out.println("dir:" + dir);
+                    RandomAccessFile inFile = new RandomAccessFile(dir + separator + file, "rw");
+                    Socket sock = new Socket(host, ip);
+                    InputStream inStor = sock.getInputStream();
+                    byte bb[] = new byte[1024];
+                    int amount;
+                    try {
+                        while ((amount = inStor.read(bb)) != -1) {
+                            inFile.write(bb, 0, amount);
+                        }
+                        inStor.close();
+                        out.println("226 transfer complete");
+                        inFile.close();
+                        sock.close();
+                    } catch (IOException e) {
+                    }
+                }
+
+                if (line.startsWith("QUIT")) {
+                    out.println("Goodbye");
+                    done = true;
+                }
+            }
+
+            client.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ftpserver/src/main/java/ftpserver/Main.java
@@ -0,0 +1,21 @@
+package ftpserver;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    private static final int PORT = 8821;
+    private static final int SO_TIMEOUT = 0; // msec
+
+    public static void main(String[] args) throws Exception {
+        try(ServerSocket server = new ServerSocket(PORT)) {
+            server.setSoTimeout(SO_TIMEOUT);
+
+            for(;;){
+                Socket client = server.accept();
+                FtpServerThread ftpServerThread = new FtpServerThread(client);
+                new Thread(ftpServerThread).start();
+            }
+        }
+    }
+}
