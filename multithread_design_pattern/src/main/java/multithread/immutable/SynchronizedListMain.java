b07f12b8df3c4e65c88cf4589a0d63d9155e178d
purple <shuggie.otis0@gmail.com>
2017-02-07 02:52:08 +0900

    add immutable examples

---
+++ multithread_design_pattern/src/main/java/multithread/immutable/SynchronizedListMain.java
@@ -0,0 +1,31 @@
+package multithread.immutable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SynchronizedListMain {
+    public static void main(String[] args) {
+        final List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        new Thread(new User(list)).start();
+        new Thread(new User(list)).start();
+        new Thread(new User(list)).start();
+    }
+
+    static class User implements Runnable {
+        private final List<Integer> list;
+
+        public User(List<Integer> list) {
+            this.list = list;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000000; i++) {
+                list.add(1);
+                list.get(0);
+                list.remove(0);
+            }
+        }
+    }
+}
