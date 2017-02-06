6b00c1deb6abfe660122dfb54119133c87173463
pppurple <shuggie.otis0@gmail.com>
2017-02-06 22:03:15 +0900

    update

--- multithread_design_pattern/src/main/java/multithread/introduction/MyClass.java
+++ multithread_design_pattern/src/main/java/multithread/introduction/MyClass.java
@@ -1,27 +1,63 @@
 package multithread.introduction;
 
 public class MyClass {
-    public void myMethod() {
-        System.out.println("myMethod");
+    public void myInstanceMethod() {
+        System.out.println(Thread.currentThread().getName() + ": instanceMethod start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": instanceMethod end");
     }
 
-    public synchronized void syncMethodA() {
-        System.out.println("syncMethodA");
+    public synchronized void syncInstanceMethodA() {
+        System.out.println(Thread.currentThread().getName() + ": syncInstanceMethodA start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": syncInstanceMethodA end");
     }
 
-    public synchronized void syncMethodB() {
-        System.out.println("syncMethodB");
+    public synchronized void syncInstanceMethodB() {
+        System.out.println(Thread.currentThread().getName() + ": syncInstanceMethodB start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": syncInstanceMethodB end");
     }
 
     public static void myStaticMethod() {
-        System.out.println("myMethod");
+        System.out.println(Thread.currentThread().getName() + ": staticMethod start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": staticMethod end");
     }
 
     public static synchronized void syncStaticMethodA() {
-        System.out.println("syncStaticMethodA");
+        System.out.println(Thread.currentThread().getName() + ": syncStaticMethodA start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": syncStaticMethodA end");
     }
 
     public static synchronized void syncStaticMethodB() {
-        System.out.println("syncStaticMethodB");
+        System.out.println(Thread.currentThread().getName() + ": syncStaticMethodB start");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + ": syncStaticMethodB end");
     }
 }
--- multithread_design_pattern/src/test/java/multithread/introduction/MyClassTest.java
+++ multithread_design_pattern/src/test/java/multithread/introduction/MyClassTest.java
@@ -4,104 +4,114 @@ import org.junit.Test;
 
 public class MyClassTest {
     @Test
-    public void 同時実行可_インスタンスメソッド() {
+    public void 同時実行可_インスタンスメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable task = clazz::myMethod;
+        Runnable task = clazz::myInstanceMethod;
 
         new Thread(task).start();
         new Thread(task).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行可_インスタンスメソッドとsynchronizedメソッド() {
+    public void 同時実行可_インスタンスメソッドとsynchronizedメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable taskNonSync = clazz::myMethod;
-        Runnable taskSync = clazz::syncMethodA;
+        Runnable taskNonSync = clazz::myInstanceMethod;
+        Runnable taskSync = clazz::syncInstanceMethodA;
 
         new Thread(taskNonSync).start();
         new Thread(taskSync).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行不可_synchronizedメソッド() {
+    public void 同時実行不可_synchronizedメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable taskSync = clazz::syncMethodA;
+        Runnable taskSync = clazz::syncInstanceMethodA;
 
         new Thread(taskSync).start();
         new Thread(taskSync).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行不可_異なるsynchronizedメソッド() {
+    public void 同時実行不可_異なるsynchronizedメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable taskSyncA = clazz::syncMethodA;
-        Runnable taskSyncB = clazz::syncMethodB;
+        Runnable taskSyncA = clazz::syncInstanceMethodA;
+        Runnable taskSyncB = clazz::syncInstanceMethodB;
 
         new Thread(taskSyncA).start();
         new Thread(taskSyncB).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行可_異なるインスタンスのsynchronizedメソッド() {
+    public void 同時実行可_異なるインスタンスのsynchronizedメソッド() throws InterruptedException {
         MyClass clazz1 = new MyClass();
         MyClass clazz2 = new MyClass();
-        Runnable taskClazz1 = clazz1::syncMethodA;
-        Runnable taskClazz2 = clazz2::syncMethodA;
+        Runnable taskClazz1 = clazz1::syncInstanceMethodA;
+        Runnable taskClazz2 = clazz2::syncInstanceMethodA;
 
         new Thread(taskClazz1).start();
         new Thread(taskClazz2).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行可_異なるインスタンスの異なるsynchronizedメソッド() {
+    public void 同時実行可_異なるインスタンスの異なるsynchronizedメソッド() throws InterruptedException {
         MyClass clazz1 = new MyClass();
         MyClass clazz2 = new MyClass();
-        Runnable taskClazz1 = clazz1::syncMethodA;
-        Runnable taskClazz2 = clazz2::syncMethodB;
+        Runnable taskClazz1 = clazz1::syncInstanceMethodA;
+        Runnable taskClazz2 = clazz2::syncInstanceMethodB;
 
         new Thread(taskClazz1).start();
         new Thread(taskClazz2).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行可_synchronizedメソッドとstaticメソッド() {
+    public void 同時実行可_synchronizedメソッドとstaticメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable taskSync = clazz::syncMethodA;
+        Runnable taskSync = clazz::syncInstanceMethodA;
         Runnable taskStatic = MyClass::myStaticMethod;
 
         new Thread(taskSync).start();
         new Thread(taskStatic).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行可_synchronizedメソッドとstaticSynchronizedメソッド() {
+    public void 同時実行可_synchronizedメソッドとstaticSynchronizedメソッド() throws InterruptedException {
         MyClass clazz = new MyClass();
-        Runnable taskSync = clazz::syncMethodA;
+        Runnable taskSync = clazz::syncInstanceMethodA;
         Runnable taskStaticSync = MyClass::syncStaticMethodA;
 
         new Thread(taskSync).start();
         new Thread(taskStaticSync).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行不可_staticSynchronizedメソッド() {
+    public void 同時実行不可_staticSynchronizedメソッド() throws InterruptedException {
         Runnable taskStaticSyncA = MyClass::syncStaticMethodA;
 
         new Thread(taskStaticSyncA).start();
         new Thread(taskStaticSyncA).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行不可_異なるstaticSynchronizedメソッド() {
+    public void 同時実行不可_異なるstaticSynchronizedメソッド() throws InterruptedException {
         Runnable taskStaticSyncA = MyClass::syncStaticMethodA;
         Runnable taskStaticSyncB = MyClass::syncStaticMethodB;
 
         new Thread(taskStaticSyncA).start();
         new Thread(taskStaticSyncB).start();
+        Thread.sleep(5000);
     }
 
     @Test
-    public void 同時実行不可_異なるインスタンスから異なるstaticSynchronizedメソッド() {
+    public void 同時実行不可_異なるインスタンスから異なるstaticSynchronizedメソッド() throws InterruptedException {
         MyClass clazz1 = new MyClass();
         MyClass clazz2 = new MyClass();
         Runnable taskStaticSync1 = () -> {
@@ -113,5 +123,6 @@ public class MyClassTest {
 
         new Thread(taskStaticSync1).start();
         new Thread(taskStaticSync2).start();
+        Thread.sleep(5000);
     }
 }
