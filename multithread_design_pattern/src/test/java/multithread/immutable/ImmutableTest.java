467d7c8ae087afeb664c85b8f311d385ab59fcf0
pppurple <shuggie.otis0@gmail.com>
2017-02-22 18:37:10 +0900

    add immutable test

---
+++ multithread_design_pattern/src/test/java/multithread/immutable/ImmutableTest.java
@@ -0,0 +1,4 @@
+package multithread.immutable;
+
+public class ImmutableTest {
+}
---
+++ multithread_design_pattern/src/test/java/multithread/introduction/MyMutable.java
@@ -0,0 +1,27 @@
+package multithread.introduction;
+
+public class MyMutable {
+    public String name;
+    public int age;
+
+    public MyMutable(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
