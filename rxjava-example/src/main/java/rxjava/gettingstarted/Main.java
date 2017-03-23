25543c442b2a2c28532568e58f4fa71ee2c48dd3
purple <shuggie.otis0@gmail.com>
2017-03-24 03:25:49 +0900

    add getting started

--- rxjava-example/src/main/java/rxjava/gettingstarted/Main.java
+++ rxjava-example/src/main/java/rxjava/gettingstarted/Main.java
@@ -1,9 +1,45 @@
 package rxjava.gettingstarted;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) {
-        Flowable.just("Hello World").subscribe(System.out::println);
+    public static void main(String[] args) throws InterruptedException {
+        Flowable<Integer> flowable = Flowable.create(flowableEmitter -> {
+            IntStream.rangeClosed(1, 5)
+                    .forEach(flowableEmitter::onNext);
+            flowableEmitter.onComplete();
+        }, BackpressureStrategy.BUFFER);
+
+        flowable.subscribe(new Subscriber<Integer>() {
+            private Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println("onSubscribe");
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer num) {
+                System.out.println("onNext: " + num);
+                subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        });
     }
 }
