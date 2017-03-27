436a192e05c4caa7ea3262deab667cf113796c32
purple <shuggie.otis0@gmail.com>
2017-03-28 03:56:06 +0900

    add Operator test

--- rxjava-example/src/test/java/rxjava/operator/OperatorTest.java
+++ rxjava-example/src/test/java/rxjava/operator/OperatorTest.java
@@ -4,11 +4,15 @@ import io.reactivex.Flowable;
 import org.junit.Test;
 import rxjava.base.DebugSubscriber;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class OperatorTest {
     @Test
-    public void fromTest() {
+    public void justTest() {
         Flowable<String> flowable = Flowable.just("A", "B", "C", "D", "E");
 
         flowable.subscribe(new DebugSubscriber<String>());
@@ -23,6 +27,60 @@ public class OperatorTest {
 
     @Test
     public void fromCallableTest() {
+        Flowable<Long> flowable = Flowable.fromCallable(System::currentTimeMillis);
+
+        flowable.subscribe(new DebugSubscriber<Long>());
+    }
+
+    @Test
+    public void rangeTest() {
+        Flowable<Integer> flowable = Flowable.range(5, 3);
+
+        flowable.subscribe(new DebugSubscriber<Integer>());
+
+        Flowable<Long> flowableLong = Flowable.rangeLong(8L, 3L);
+
+        flowableLong.subscribe(new DebugSubscriber<Long>());
+    }
+
+    @Test
+    public void intervalTest() throws InterruptedException {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:ss.SSS");
+
+        System.out.println("start: " + LocalTime.now().format(formatter));
+
+        Flowable<Long> flowable = Flowable.interval(1_000L, TimeUnit.MILLISECONDS);
+
+        flowable.subscribe(data -> {
+            System.out.println(Thread.currentThread().getName()
+                    + ": " + LocalTime.now().format(formatter)
+                    + ": data=" + data);
+        });
+
+        Thread.sleep(5_000L);
+    }
+
+    @Test
+    public void timerTest() throws InterruptedException {
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm:ss.SSS");
+
+        System.out.println("start: " + LocalTime.now().format(formatter));
+
+        Flowable<Long> flowable = Flowable.timer(1_000L, TimeUnit.MILLISECONDS);
+
+        flowable.subscribe(data -> {
+                    System.out.println(Thread.currentThread().getName()
+                            + ": " + LocalTime.now().format(formatter)
+                            + ": data=" + data);
+                },
+                error -> System.out.println("error=" + error),
+                () -> System.out.println("complete"));
+
+        Thread.sleep(1_500L);
+    }
+
+    @Test
+    public void deferTest() {
 
     }
 }
