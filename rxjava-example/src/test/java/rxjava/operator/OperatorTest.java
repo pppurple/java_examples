056ef1a08662d4cf7674b06d62a80121fcc494a8
purple <shuggie.otis0@gmail.com>
2017-03-30 03:40:12 +0900

    add test case

--- rxjava-example/src/test/java/rxjava/operator/OperatorTest.java
+++ rxjava-example/src/test/java/rxjava/operator/OperatorTest.java
@@ -6,6 +6,7 @@ import rxjava.base.DebugSubscriber;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
@@ -117,6 +118,100 @@ public class OperatorTest {
 
     @Test
     public void flatMapTest() {
+        // flatMap(mapper)
+        Flowable<String> flowable = Flowable.just("A", "", "C", "", "E")
+                .flatMap(data -> {
+                    if ("".equals(data)) {
+                        return Flowable.empty();
+                    } else {
+                        return Flowable.just(data.toLowerCase());
+                    }
+                });
+        flowable.subscribe(new DebugSubscriber<String>());
 
+        // flatMap(mapper, combiner)
+        Flowable<String> flowable2 = Flowable.range(1, 3)
+                .flatMap(data -> {
+                            return Flowable.interval(100L, TimeUnit.MILLISECONDS)
+                                    .take(3);
+                        },
+                        (sourceData, newData) -> "[" + sourceData + "]" + newData);
+        flowable2.subscribe(new DebugSubscriber<String>());
+
+        // flatMap(oneNextMapper, onErrorMapper, onCompleteSupplier)
+        Flowable<Integer> original = Flowable.just(1, 2, 0, 4, 5)
+                .map(data -> 10 / data); // throw exception
+
+        Flowable<Integer> flowable3 = original.flatMap(
+                data -> Flowable.just(data),
+                error -> Flowable.just(-1),
+                () -> Flowable.just(100)
+        );
+        flowable3.subscribe(new DebugSubscriber<Integer>());
     }
+
+    @Test
+    public void concatMapTest() throws InterruptedException {
+        // concatMap(mapper)
+        Flowable<String> flowable = Flowable.range(10, 3)
+                .concatMap(
+                        sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                                .take(2)
+                                .map(data -> {
+                                    long time = System.currentTimeMillis();
+                                    return time + "ms: [" + sourceData + "]" + data;
+                                }));
+        flowable.subscribe(new DebugSubscriber<String>());
+        Thread.sleep(4_000L);
+    }
+
+    @Test
+    public void concatMapEagerTest() throws InterruptedException {
+        // concatMapEager(mapper)
+        Flowable<String> flowable = Flowable.range(10, 3)
+                .concatMapEager(
+                        sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                                .take(2)
+                                .map(data -> {
+                                    long time = System.currentTimeMillis();
+                                    return time + "ms: [" + sourceData + "]" + data;
+                                }));
+        flowable.subscribe(new DebugSubscriber<String>());
+        Thread.sleep(4_000L);
+    }
+
+    @Test
+    public void concatMapEagerDelayErrorTest() throws InterruptedException {
+        Flowable<String> flowable = Flowable.range(10, 3)
+                .concatMapEagerDelayError(
+                        sourceData -> Flowable.interval(500L, TimeUnit.MILLISECONDS)
+                                .take(3)
+                                .doOnNext(data -> {
+                                    if (sourceData == 11 && data == 1) {
+                                        throw new Exception("Exception!!");
+                                    }
+                                })
+                                .map(data -> "[" + sourceData + "]" + data),
+                        true);
+        flowable.subscribe(new DebugSubscriber<String>());
+        Thread.sleep(4_000L);
+    }
+
+    @Test
+    public void bufferTest() throws InterruptedException {
+        // buffer(count)
+        Flowable<List<Long>> flowable = Flowable.interval(100L, TimeUnit.MILLISECONDS)
+                .take(10)
+                .buffer(3);
+        flowable.subscribe(new DebugSubscriber<List<Long>>());
+        Thread.sleep(3_000L);
+
+        // buffer(boundaryIndicatorSupplier)
+        Flowable<List<Long>> flowable2 = Flowable.interval(300L, TimeUnit.MILLISECONDS)
+                .take(7)
+                .buffer(() -> Flowable.timer(1_000L, TimeUnit.MILLISECONDS));
+        flowable2.subscribe(new DebugSubscriber<List<Long>>());
+        Thread.sleep(4_000L);
+    }
+
 }
