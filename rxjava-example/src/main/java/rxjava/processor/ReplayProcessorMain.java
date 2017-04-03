8da84286303f5aedc91221e75887ef2584450816
purple <shuggie.otis0@gmail.com>
2017-04-04 03:59:18 +0900

    add processor

---
+++ rxjava-example/src/main/java/rxjava/processor/ReplayProcessorMain.java
@@ -0,0 +1,27 @@
+package rxjava.processor;
+
+import io.reactivex.processors.ReplayProcessor;
+import rxjava.base.DebugSubscriber;
+
+public class ReplayProcessorMain {
+    public static void main(String[] args) {
+        ReplayProcessor<Integer> processor = ReplayProcessor.create();
+
+        processor.subscribe(new DebugSubscriber<Integer>("No.1"));
+
+        processor.onNext(1);
+        processor.onNext(2);
+        processor.onNext(3);
+
+        System.out.println("add Subscriber No.2");
+        processor.subscribe(new DebugSubscriber<Integer>("--No.2"));
+
+        processor.onNext(4);
+        processor.onNext(5);
+
+        processor.onComplete();
+
+        System.out.println("add Subscriber No.3");
+        processor.subscribe(new DebugSubscriber<Integer>("----No.3"));
+    }
+}
