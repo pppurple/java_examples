df1d68b52c19099778944c32ac982a11c3521a53
purple <shuggie.otis0@gmail.com>
2017-06-08 01:56:54 +0900

    update

--- javase8/src/test/java/javase8/CompletableFutureTest.java
+++ javase8/src/test/java/javase8/CompletableFutureTest.java
@@ -15,6 +15,8 @@ import java.util.concurrent.FutureTask;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CompletableFutureTest {
     private static AtomicInteger atomicInt;
 
@@ -35,9 +37,9 @@ public class CompletableFutureTest {
 
         Future<Integer> future = executor.submit(callable);
         int num = future.get();
-        int result = num + 100;
+        int doubled = NumUtil.doubleNum(num);
 
-        System.out.println(result);
+        assertThat(doubled).isEqualTo(2);
     }
 
     @Test
@@ -46,18 +48,33 @@ public class CompletableFutureTest {
             int generated = atomicInt.incrementAndGet();
             try {
                 Thread.sleep(3_000L);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException ignored) {
             }
             return generated;
         };
 
         CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(supplier);
 
-        int result = completableFuture.thenApply(num -> num + 100).get();
+        int doubled = completableFuture.thenApply(NumUtil::doubleNum).get();
 
-        System.out.println(result);
+        assertThat(doubled).isEqualTo(2);
     }
 
+    @Test
+    public void thenAcceptTest() throws ExecutionException, InterruptedException {
+        Supplier<Integer> supplier = () -> {
+            int generated = atomicInt.incrementAndGet();
+            try {
+                Thread.sleep(3_000L);
+            } catch (InterruptedException ignored) {
+            }
+            return generated;
+        };
+
+        CompletableFuture.supplyAsync(supplier)
+                .thenApply(NumUtil::doubleNum)
+                .thenAccept(System.out::println);
+    }
     @Test
     public void test() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
@@ -117,4 +134,9 @@ public class CompletableFutureTest {
                 .forEach(System.out::println);
     }
 
+    private static class NumUtil {
+        static int doubleNum(int num) {
+            return 2 * num;
+        }
+    }
 }
