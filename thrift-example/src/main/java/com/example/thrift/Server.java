8332730e0242409dbf319c68ae149b5c870bfb19
pppurple <shuggie.otis0@gmail.com>
2017-12-11 21:32:25 +0900

    Add simple

---
+++ thrift-example/src/main/java/com/example/thrift/Server.java
@@ -0,0 +1,59 @@
+package com.example.thrift;
+
+import com.example.thrift.PeopleService.Processor;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            Processor<PeopleService.Iface> processor = new Processor<>(new PeopleService.Iface() {
+                @Override
+                public List<Person> searchByName(String query) {
+
+                    // search by query
+                    // PeopleDao.search(query)
+                    Person alice = new Person("Alice", 33, Country.findByValue(1), "trick");
+                    Person bobby = new Person("Bobby", 29, Country.findByValue(0), "yo-yo");
+                    List<Person> people = Arrays.asList(alice, bobby);
+
+                    return people;
+                }
+            });
+
+            // written with lambda
+            /*
+            Processor<PeopleService.Iface> processor = new Processor<>(query -> {
+
+                // search by query
+                // PeopleDao.search(query)
+                Person alice = new Person("Alice", 33, Country.findByValue(1), "trick");
+                Person bobby = new Person("Bobby", 29, Country.findByValue(0), "yo-yo");
+                List<Person> people = Arrays.asList(alice, bobby);
+
+                return people;
+            });
+            */
+
+            TServerTransport serverTransport = new TServerSocket(8080);
+
+            TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+            // Use this for a multithreaded server
+            // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+
+            System.out.println("Starting the server...");
+
+            new Thread(server::serve).start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
