bb531f4208c43df5e3be5303d9f87e4b35b9eb3b
pppurple <shuggie.otis0@gmail.com>
2018-02-22 21:38:49 +0900

    Fixed Main

--- fluent_logger_example/src/main/java/com/example/logger/fluent/Main.java
+++ fluent_logger_example/src/main/java/com/example/logger/fluent/Main.java
@@ -2,13 +2,10 @@ package com.example.logger.fluent;
 
 import org.fluentd.logger.FluentLogger;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    // private static FluentLogger LOGGER = FluentLogger.getLogger("app");
-    // private static FluentLogger LOGGER = FluentLogger.getLogger("app", "127.0.0.1", 24224);
     private static FluentLogger LOGGER = FluentLogger.getLogger("app", "localhost", 24224);
 
     public static void main(String[] args) {
@@ -16,9 +13,7 @@ public class Main {
         data.put("name", "Andy");
         data.put("age", "21");
         data.put("country", "America");
-        data.put("timestamp", new Date());
 
-        boolean result = LOGGER.log("user", data);
-        System.out.println(result);
+        LOGGER.log("user", data);
     }
 }
