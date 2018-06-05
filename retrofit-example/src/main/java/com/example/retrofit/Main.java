c146ad50621f886e65807f743256359d341f5a97
pppurple <shuggie.otis0@gmail.com>
2018-06-05 22:03:22 +0900

    add interface

---
+++ retrofit-example/src/main/java/com/example/retrofit/Main.java
@@ -0,0 +1,4 @@
+package com.example.retrofit;
+
+public class Main {
+}
---
+++ retrofit-example/src/main/java/com/example/retrofit/client/MyService.java
@@ -0,0 +1,52 @@
+package com.example.retrofit.client;
+
+import com.example.retrofit.model.Dog;
+import retrofit2.Call;
+import retrofit2.http.*;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MyService {
+    @GET("dog")
+    Call<Dog> getDog();
+
+    @GET("dogs")
+    Call<List<Dog>> getDogList();
+
+    @GET("dog?aaa=bbb")
+    Call<Dog> getDogWithParam();
+
+    @GET("dog")
+    Call<Dog> getDogWithParam(@Query("aaa") String value);
+
+    @GET("dog")
+    Call<Dog> getDogWithParams(@QueryMap Map<String, String> params);
+
+    @Headers({
+            "User-Agent: my-service:0.1",
+            "my-header: zzz"
+    })
+    @GET("dog")
+    Call<Dog> getDogWithHeaders();
+
+    @GET("dog")
+    Call<Dog> getDogWithDynamicHeader(@Header("User-Agent") String userAgent);
+
+    @GET("dogs/{id}")
+    Call<Dog> getDogById();
+
+    @POST("dogs")
+    Call<Void> create(@Body Dog dog);
+
+    @FormUrlEncoded
+    @POST("dogs/form")
+    Call<Void> form(@Field("id") String id,
+                    @Field("name") String name);
+
+    @PUT("dogs/{id}")
+    Call<Void> update(@Body Dog dog);
+
+    @DELETE("dogs/{id}")
+    Call<Void> delete();
+}
