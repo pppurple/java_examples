d3f7d09fac2b0a2df9ba0ed2bfa73172aeca335c
pppurple <shuggie.otis0@gmail.com>
2018-06-05 22:03:10 +0900

    Add model

---
+++ retrofit-example/src/main/java/com/example/retrofit/model/Dog.java
@@ -0,0 +1,13 @@
+package com.example.retrofit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dog {
+    private int id;
+    private String name;
+}
