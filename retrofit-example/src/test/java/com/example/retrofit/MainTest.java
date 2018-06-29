4ffe179fda49e6626a7b56598f4a48c3bf8eb4d8
pppurple <shuggie.otis0@gmail.com>
2018-06-29 21:41:02 +0900

    Use jackson converter

--- retrofit-example/src/test/java/com/example/retrofit/MainTest.java
+++ retrofit-example/src/test/java/com/example/retrofit/MainTest.java
@@ -2,10 +2,13 @@ package com.example.retrofit;
 
 import com.example.retrofit.client.MyService;
 import com.example.retrofit.model.Dog;
+import okhttp3.OkHttpClient;
+import okhttp3.internal.http.HttpMethod;
 import org.junit.Before;
 import org.junit.Test;
-import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class MainTest {
     private MyService myService;
@@ -14,14 +17,22 @@ public class MainTest {
     public void before() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://localhost:8080/")
+//                .client(new OkHttpClient())
+                .addConverterFactory(JacksonConverterFactory.create())
                 .build();
 
         myService = retrofit.create(MyService.class);
     }
 
     @Test
-    public void getTest() {
-        Call<Dog> dogCall = myService.get();
-
+    public void getTest() throws Exception {
+        Response<Dog> response = myService.getDog().execute();
+        if (!response.isSuccessful()) {
+            System.out.println("error!!" + response.errorBody());
+            String url = response.raw().request().url().toString();
+            System.out.println("url=" + url + ". ");
+        }
+        Dog dog = response.body();
+        System.out.println("dog: " + dog);
     }
 }
