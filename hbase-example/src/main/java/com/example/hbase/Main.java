6bd6d35b334ed63787d989184cd1c23415c6f948
purple <shuggie.otis0@gmail.com>
2018-07-30 05:00:25 +0900

    add Main

--- hbase-example/src/main/java/com/example/hbase/Main.java
+++ hbase-example/src/main/java/com/example/hbase/Main.java
@@ -1,4 +1,18 @@
 package com.example.hbase;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.HTableInterface;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        Configuration conf = HBaseConfiguration.create();
+        HTable hTable = new HTable(conf, "");
+
+
+        hTable.close();
+    }
 }
