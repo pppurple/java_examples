b137be0002d8f7cd13ec9462959e518fcd423ba5
purple <shuggie.otis0@gmail.com>
2018-10-03 02:52:39 +0900

    fixed producer

--- kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -7,34 +7,30 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         // configuration
         Properties properties = new Properties();
-//        properties.put("zk.connect", "127.0.0.1:2181");
-//        properties.put("bootstrap.servers", "127.0.0.1:9092");
-//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:9092");
-//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.2.15:9092");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        // sync send
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-//        ProducerRecord<String, String> record = new ProducerRecord<>("test", "my first send 1");
-//        ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "message", "my first send 1");
-        ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "my first send 1");
-        try {
-            Future<RecordMetadata> send = producer.send(record);
-            System.out.println(send.get());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    private static void syncSend() {
+        IntStream.range(0, 10)
+                .forEach(i -> {
+                    ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "my topic-" + i);
+                    try {
+                        // sync send
+                        Future<RecordMetadata> send = producer.send(record);
+                        System.out.println(send.get());
 
+                        Thread.sleep(1_000L);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
