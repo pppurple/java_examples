e58426641f3aee9d21bd3cf8cc19d4fd10b4e45c
purple <shuggie.otis0@gmail.com>
2018-10-04 02:53:49 +0900

    moved from Main

--- kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -1,36 +1,18 @@
 package com.example.producer.client.kafka;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-
-import java.util.Properties;
-import java.util.concurrent.Future;
-import java.util.stream.IntStream;
+import com.example.producer.client.kafka.producer.AsyncSendProducer;
+import com.example.producer.client.kafka.producer.SyncSendProducer;
 
 public class Main {
     public static void main(String[] args) {
-        // configuration
-        Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        // sync send
+        /*
+        SyncSendProducer syncSendProducer = new SyncSendProducer();
+        syncSendProducer.syncSend();
+        */
 
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-
-        IntStream.range(0, 10)
-                .forEach(i -> {
-                    ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "my_value-" + i);
-                    try {
-                        // sync send
-                        Future<RecordMetadata> send = producer.send(record);
-                        System.out.println(send.get());
-
-                        Thread.sleep(1_000L);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+        // async send
+        AsyncSendProducer asyncSendProducer = new AsyncSendProducer();
+        asyncSendProducer.asyncSend();
     }
 }
---
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/producer/SyncSendProducer.java
@@ -0,0 +1,36 @@
+package com.example.producer.client.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+public class SyncSendProducer {
+    public void syncSend() {
+        // configuration
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        IntStream.range(0, 10)
+                .forEach(i -> {
+                    ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "my_value-" + i);
+                    try {
+                        // sync send
+                        Future<RecordMetadata> send = producer.send(record);
+                        System.out.println(send.get());
+
+                        Thread.sleep(1_000L);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
