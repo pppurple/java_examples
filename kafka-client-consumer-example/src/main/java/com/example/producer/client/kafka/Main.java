5710d02a34878619f98fe271fff4d91ffa1cfb19
purple <shuggie.otis0@gmail.com>
2018-10-10 02:16:13 +0900

    rename

--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -1,13 +1,15 @@
 package com.example.producer.client.kafka;
 
-import com.example.producer.client.kafka.consumer.AutoCommitConsumer;
-import com.example.producer.client.kafka.consumer.SeekToBeginningConsumer;
+import com.example.producer.client.kafka.consumer.ManualSyncCommitConsumer;
 
 public class Main {
     public static void main(String[] args) {
         // consumer
-        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
-        autoCommitConsumer.consume();
+/*        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
+        autoCommitConsumer.consume();*/
+
+        ManualSyncCommitConsumer manualSyncCommitConsumer = new ManualSyncCommitConsumer();
+        manualSyncCommitConsumer.consume();
 
         // seek to beginning
         /*
