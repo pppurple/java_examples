efc6833acd821eba75d30f2be00b15181d6b3799
purple <shuggie.otis0@gmail.com>
2018-10-13 19:10:10 +0900

    removed unnecessary comment

--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -1,20 +1,17 @@
 package com.example.producer.client.kafka;
 
+import com.example.producer.client.kafka.consumer.AutoCommitConsumer;
 import com.example.producer.client.kafka.consumer.ManualSyncCommitConsumer;
 
 public class Main {
     public static void main(String[] args) {
         // consumer
-/*        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
-        autoCommitConsumer.consume();*/
+        /*
+        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
+        autoCommitConsumer.consume();
+        */
 
         ManualSyncCommitConsumer manualSyncCommitConsumer = new ManualSyncCommitConsumer();
         manualSyncCommitConsumer.consume();
-
-        // seek to beginning
-        /*
-        SeekToBeginningConsumer seekToBeginningConsumer = new SeekToBeginningConsumer();
-        seekToBeginningConsumer.seekToStart();
-        */
     }
 }
