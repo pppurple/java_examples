c93eafadf4840e3813cbae4abb79f770d10911db
purple <shuggie.otis0@gmail.com>
2018-11-13 03:11:39 +0900

    add seek to beginning consumer examples

---
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumer/SeekToBeginningConsumer.java
@@ -0,0 +1,47 @@
+package com.example.producer.client.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class SeekToBeginningConsumer {
+    public void seekToStart() {
+        // configuration
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "myConsumerGroup");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        List<TopicPartition> topicPartitions = new ArrayList<>();
+        for (PartitionInfo partitionInfo : consumer.partitionsFor("my1")) {
+            topicPartitions.add(new TopicPartition(partitionInfo.topic(), partitionInfo.partition()));
+        }
+
+        // seek from first
+        consumer.assign(topicPartitions);
+        consumer.seekToBeginning(consumer.assignment());
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1_000));
+
+                records.forEach(record -> {
+                    System.out.println("partition: " + record.partition() + ", topic: " + record.topic() + ", key: " + record.key() + ", value: " + record.value());
+                });
+            }
+        } finally {
+            consumer.close();
+        }
+
+    }
+}
