5b68a1756af6a4c005d784d152ed9960f56553e0
purple <shuggie.otis0@gmail.com>
2018-11-20 03:14:37 +0900

    polish

--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumer/SeekToBeginningConsumer.java
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumer/SeekToBeginningConsumer.java
@@ -42,6 +42,5 @@ public class SeekToBeginningConsumer {
         } finally {
             consumer.close();
         }
-
     }
 }
--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumergroup/ConsumeTask.java
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/consumergroup/ConsumeTask.java
@@ -18,22 +18,6 @@ public class ConsumeTask implements Runnable {
         this("myConsumerGroup", consumerName, topic);
     }
 
-    /*
-    public ConsumeTask(String consumerName, String topic) {
-        this.consumerGroupName = "myConsumerGroup";
-        this.consumerName = consumerName;
-        this.topic = topic;
-
-        // configuration
-        Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupName);
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        this.consumer = new KafkaConsumer<>(properties);
-    }
-    */
-
     public ConsumeTask(String consumerGroupName, String consumerName, String topic) {
         this.consumerGroupName = consumerGroupName;
         this.consumerName = consumerName;
@@ -56,8 +40,15 @@ public class ConsumeTask implements Runnable {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1_000));
 
+                // print information about topic record
                 records.forEach(record -> {
-                    System.out.println("group: " + consumerGroupName + ", consumer: " + consumerName + ", partition: " + record.partition() + ", topic: " + record.topic() + ", key: " + record.key() + ", value: " + record.value());
+                    System.out.println("group: " + consumerGroupName
+                            + ", consumer: " + consumerName
+                            + ", partition: " + record.partition()
+                            + ", topic: " + record.topic()
+                            + ", key: " + record.key()
+                            + ", value: " + record.value()
+                    );
                 });
             }
         } finally {
