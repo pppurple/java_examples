31572650b74764e545c945239796d7319fedcb26
purple <shuggie.otis0@gmail.com>
2018-11-20 03:22:55 +0900

    fixed main

--- kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-consumer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -3,21 +3,22 @@ package com.example.producer.client.kafka;
 import com.example.producer.client.kafka.consumer.AutoCommitConsumer;
 import com.example.producer.client.kafka.consumer.ConsumerWithPartition;
 import com.example.producer.client.kafka.consumer.ManualSyncCommitConsumer;
+import com.example.producer.client.kafka.consumer.SeekToAnyOffsetConsumer;
 import com.example.producer.client.kafka.consumer.SeekToBeginningConsumer;
 
 public class Main {
     public static void main(String[] args) {
         // auto commit
-//        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
-//        autoCommitConsumer.consume();
+        AutoCommitConsumer autoCommitConsumer = new AutoCommitConsumer();
+        autoCommitConsumer.consume();
 
         // manual commit
-//        ManualSyncCommitConsumer manualSyncCommitConsumer = new ManualSyncCommitConsumer();
-//        manualSyncCommitConsumer.consume();
+        ManualSyncCommitConsumer manualSyncCommitConsumer = new ManualSyncCommitConsumer();
+        manualSyncCommitConsumer.consume();
 
         // consume with partition
-//        ConsumerWithPartition consumerWithPartition = new ConsumerWithPartition();
-//        consumerWithPartition.consumeFromEachPartition();
+        ConsumerWithPartition consumerWithPartition = new ConsumerWithPartition();
+        consumerWithPartition.consumeFromEachPartition();
 
         // seek to beginning
         SeekToBeginningConsumer seekToBeginningConsumer = new SeekToBeginningConsumer();
