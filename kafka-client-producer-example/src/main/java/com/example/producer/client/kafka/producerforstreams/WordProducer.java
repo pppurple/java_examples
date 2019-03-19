67913cb7eb16bf7f917f5b0b4cb2248905c8f5b0
purple <shuggie.otis0@gmail.com>
2019-03-20 03:15:52 +0900

    add WordProducer

---
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/producerforstreams/WordProducer.java
@@ -0,0 +1,45 @@
+package com.example.producer.client.kafka.producerforstreams;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+public class WordProducer {
+    public void syncSend() {
+        // configuration
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String[] words = {"apple", "studio", "witch", "purple", "hollywood", "tree"};
+
+        IntStream.range(0, 100)
+                .forEach(i -> {
+                    String key = "key" + i;
+                    Random random = new Random();
+                    String word = words[random.nextInt(words.length)];
+                    ProducerRecord<String, String> record = new ProducerRecord<>("w12", key, word);
+                    try {
+                        // sync send
+                        Future<RecordMetadata> send = producer.send(record);
+                        RecordMetadata recordMetadata = send.get();
+                        System.out.println("partition: " + recordMetadata.partition() +
+                                ", topic: " + recordMetadata.topic() +
+                                ", offset: " + recordMetadata.offset() +
+                                ", key: " + key +
+                                ", value: " + word);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
