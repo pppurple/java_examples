bbdcb08a300b36fb142b97ffcb9c445e5b5872c8
purple <shuggie.otis0@gmail.com>
2019-03-21 02:52:27 +0900

    fixed serdes

--- kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/producerforstreams/IncrementalKeyProducer.java
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/producerforstreams/IncrementalKeyProducer.java
@@ -16,15 +16,15 @@ public class IncrementalKeyProducer {
         // configuration
         Properties properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.Integer().getClass());
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.Integer().serializer().getClass());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass());
 
         KafkaProducer<Integer, String> producer = new KafkaProducer<>(properties);
 
         IntStream.range(0, 100)
                 .forEach(i -> {
                     String value = RandomStringUtils.randomAlphabetic(5);
-                    ProducerRecord<Integer, String> record = new ProducerRecord<>("s1", i,  value);
+                    ProducerRecord<Integer, String> record = new ProducerRecord<>("basic", i,  value);
                     try {
                         // sync send
                         Future<RecordMetadata> send = producer.send(record);
--- kafka-streams-example/src/main/java/com/example/streams/kafka/stream/BasicStream.java
+++ kafka-streams-example/src/main/java/com/example/streams/kafka/stream/BasicStream.java
@@ -12,13 +12,13 @@ public class BasicStream {
     public void start() {
         // configuration
         Properties properties = new Properties();
-        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "myStream");
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "basic_stream");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder streamsBuilder = new StreamsBuilder();
-        KStream<Integer, String> kStream = streamsBuilder.stream("s11");
+        KStream<Integer, String> kStream = streamsBuilder.stream("basic");
 
         kStream.filter((key, value) -> value.matches("^[A-Z].+"))
                 .foreach(((key, value) -> System.out.println("key: " + key + ", value: " + value)));
