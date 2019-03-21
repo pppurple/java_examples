95eb7fc2974d9514e95a89645582d3c4ed282431
purple <shuggie.otis0@gmail.com>
2019-03-22 04:51:14 +0900

    add TextProducer

--- kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/Main.java
@@ -6,31 +6,41 @@ import com.example.producer.client.kafka.producer.ProducerWithDefaultPartition;
 import com.example.producer.client.kafka.producer.ProducerWithKeyWithDefaultPartition;
 import com.example.producer.client.kafka.producer.SyncSendProducer;
 import com.example.producer.client.kafka.producerforstreams.IncrementalKeyProducer;
+import com.example.producer.client.kafka.producerforstreams.TextProducer;
+import com.example.producer.client.kafka.producerforstreams.WordProducer;
 
 public class Main {
     public static void main(String[] args) {
         // sync send
-        SyncSendProducer syncSendProducer = new SyncSendProducer();
-        syncSendProducer.syncSend();
+//        SyncSendProducer syncSendProducer = new SyncSendProducer();
+//        syncSendProducer.syncSend();
 
         // async send
-        AsyncSendProducer asyncSendProducer = new AsyncSendProducer();
-        asyncSendProducer.asyncSend();
+//        AsyncSendProducer asyncSendProducer = new AsyncSendProducer();
+//        asyncSendProducer.asyncSend();
 
         // sync with no key with default partition
-        ProducerWithDefaultPartition producerWithDefaultPartition = new ProducerWithDefaultPartition();
-        producerWithDefaultPartition.syncSend();
+//        ProducerWithDefaultPartition producerWithDefaultPartition = new ProducerWithDefaultPartition();
+//        producerWithDefaultPartition.syncSend();
 
         // sync with key with default partition
-        ProducerWithKeyWithDefaultPartition producerWithKeyWithDefaultPartition = new ProducerWithKeyWithDefaultPartition();
-        producerWithKeyWithDefaultPartition.syncSend();
+//        ProducerWithKeyWithDefaultPartition producerWithKeyWithDefaultPartition = new ProducerWithKeyWithDefaultPartition();
+//        producerWithKeyWithDefaultPartition.syncSend();
 
         // sync with custom partition
-        ProducerWithCustomPartition producerWithCustomPartition = new ProducerWithCustomPartition();
-        producerWithCustomPartition.syncSend();
+//        ProducerWithCustomPartition producerWithCustomPartition = new ProducerWithCustomPartition();
+//        producerWithCustomPartition.syncSend();
 
         // incremental key producer
-        IncrementalKeyProducer incrementalKeyProducer = new IncrementalKeyProducer();
-        incrementalKeyProducer.syncSend();
+//        IncrementalKeyProducer incrementalKeyProducer = new IncrementalKeyProducer();
+//        incrementalKeyProducer.syncSend();
+
+        // word producer
+//        WordProducer wordProducer = new WordProducer();
+//        wordProducer.syncSend();
+
+        // text producer
+//        TextProducer textProducer = new TextProducer();
+//        textProducer.syncSend();
     }
 }
---
+++ kafka-client-producer-example/src/main/java/com/example/producer/client/kafka/producerforstreams/TextProducer.java
@@ -0,0 +1,49 @@
+package com.example.producer.client.kafka.producerforstreams;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
+public class TextProducer {
+    public void syncSend() {
+        // configuration
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String[] words = {
+                "The Beatles were an English rock band formed in Liverpool in 1960.",
+                "Led by primary songwriters Lennon and McCartney",
+                "the Beatles built their reputation playing clubs in Liverpool and Hamburg over a three-year period from 1960, with Stuart Sutcliffe initially serving as bass player. The core trio of Lennon, McCartney and Harrison, together since 1958, went through a succession of drummers, including Pete Best, before asking Starr to join them in 1962.",
+                "By early 1964, the Beatles were international stars, leading the British Invasion of the United States pop market and breaking numerous sales records.",
+                "The Beatles are the best-selling band in history, with estimated sales of over 800 million records worldwide."
+        };
+
+        IntStream.range(0, words.length)
+                .forEach(i -> {
+                    String key = "key" + i;
+                    String word = words[i];
+                    ProducerRecord<String, String> record = new ProducerRecord<>("sen11", key, word);
+                    try {
+                        // sync send
+                        Future<RecordMetadata> send = producer.send(record);
+                        RecordMetadata recordMetadata = send.get();
+                        System.out.println("partition: " + recordMetadata.partition() +
+                                ", topic: " + recordMetadata.topic() +
+                                ", offset: " + recordMetadata.offset() +
+                                ", key: " + key +
+                                ", value: " + word);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+}
