638d8fce7014aca5a48751a370b58668ba88c49b
purple <shuggie.otis0@gmail.com>
2019-03-29 02:58:43 +0900

    add temporary stream

---
+++ kafka-streams-example/src/main/java/com/example/streams/kafka/temporary/TemporaryWordCountStream.java
@@ -0,0 +1,29 @@
+package com.example.streams.kafka.temporary;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class TemporaryWordCountStream {
+    public static void main(String[] args) {
+        // configuration
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "my_temporary_stream");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        KStream<String, Long> kStream = streamsBuilder.stream("word-count");
+
+        kStream.foreach((k, v) -> System.out.println(k + " : " + v));
+
+        KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), properties);
+
+        streams.start();
+    }
+}
